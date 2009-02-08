3b9e45692aa05504e676df86b8fa6d9560fc4b6a
Lennart Kolmodin <kolmodin@gentoo.org>
2009-02-08 20:46:00 +0000

    Fix 'hackport diff additions' and 'hackport diff common'

--- Diff.hs
+++ Diff.hs
@@ -125,9 +125,17 @@ diff hackage overlay dm = do
                      InBoth o h -> h>o
                      _ -> False
       ShowMissing -> case st of
-                      OnlyInRight _ -> True
-                      InBoth x y -> x < y
-                      OnlyInLeft _ -> False
+                     OnlyInLeft _ -> False
+                     InBoth x y -> x < y
+                     OnlyInRight _ -> True
+      ShowAdditions -> case st of
+                     OnlyInLeft _ -> True
+                     InBoth x y -> x > y
+                     OnlyInRight _ -> False
+      ShowCommon -> case st of
+                     OnlyInLeft _ -> False
+                     InBoth x y -> x == y
+                     OnlyInRight _ -> False
       _ -> True
  
 -- | We get the 'PackageCompareInfo' by combining the info for the overlay
@@ -177,31 +185,3 @@ groupOn :: Ord key => (a -> key) -> [a] -> [(key,[a])]
 groupOn key = map (\xs -> (key (head xs), xs))
             . groupBy (equating key)
             . sortBy (comparing key)
-{-
-diff :: Portage -> Portage -> DiffMode -> IO ()
-diff pt1 pt2 mode = do
-  let pkgs1 = Map.map (OnlyLeft  . eVersion . maximum) pt1
-  let pkgs2 = Map.map (OnlyRight . eVersion . maximum) pt2
-  let union = Map.unionWith (\(OnlyLeft x) (OnlyRight y) -> Both x y) pkgs1 pkgs2
-  let showFilter st = case mode of
-          ShowAll -> True
-          ShowMissing -> case st of
-                  OnlyLeft _ -> True
-                  Both x y -> x > y
-                  OnlyRight _ -> False
-          ShowAdditions -> case st of
-                  OnlyLeft _ -> False
-                  Both x y -> x < y
-                  OnlyRight _ -> True
-          ShowNewer -> case st of
-                  OnlyLeft _ -> False
-                  Both x y -> x > y
-                  OnlyRight _ -> False
-          ShowCommon -> case st of
-                  OnlyLeft _ -> False
-                  Both x y -> x == y
-                  OnlyRight _ -> False
-          ShowPackages _ -> True
-  let packages = filter (showFilter . snd) (Map.assocs union)
-  mapM_ (putStrLn . uncurry showDiffState) packages
--}
