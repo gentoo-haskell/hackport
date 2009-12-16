70af344cd11082ec4e9befa8b596283b3cbc4e28
Lennart Kolmodin <kolmodin@gentoo.org>
2009-12-16 20:01:30 +0000

    remove the bundled cabal-install-0.6.2

--- cabal-install-0.6.2/Paths_cabal_install.hs
+++
@@ -1,9 +0,0 @@
-module Paths_cabal_install where
-
-import Data.Version (Version(..))
-
--- manually written to use cabal-install as a library
-
-version :: Version
-version = Version {versionBranch = [0,6,2], versionTags = []}
-
