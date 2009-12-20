404fcc820bc4f2ca7ddddd9432d06cbb6786058a
Sergei Trofimovich <slyfox@community.haskell.org>
2009-12-20 16:56:45 +0000

    cabal-install-0.7.5/Paths_cabal_install.hs: get rid of unused getEnv

--- cabal-install-0.8.0/Paths_cabal_install.hs
+++ cabal-install-0.8.0/Paths_cabal_install.hs
@@ -3,7 +3,6 @@ module Paths_cabal_install (
   ) where
 
 import Data.Version (Version(..))
-import System.Environment (getEnv)
 
 version :: Version
 version = Version {versionBranch = [0,8,0], versionTags = []}
