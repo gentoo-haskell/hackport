b93dfea4a7e9b2aa996b3c1f3694d9983f0ab8cd
Sergei Trofimovich <slyfox@gentoo.org>
2013-01-01 17:39:16 +0300

    Merge/Dependencies.hs: add 'crack' extralib
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- Merge/Dependencies.hs
+++ Merge/Dependencies.hs
@@ -256,6 +256,7 @@ staticTranslateExtraLib lib = lookup lib m
       , ("asound", Portage.AnyVersionOf (Portage.mkPackageName "media-libs" "alsa-lib") Portage.AnySlot [])
       , ("sqlite3", Portage.OrLaterVersionOf (Portage.Version [3,0] Nothing [] 0) (Portage.mkPackageName "dev-db" "sqlite") Portage.AnySlot [])
       , ("stdc++", Portage.AnyVersionOf (Portage.mkPackageName "sys-devel" "gcc") Portage.AnySlot [Portage.mkUse "cxx"])
+      , ("crack", Portage.AnyVersionOf (Portage.mkPackageName "sys-libs" "cracklib") Portage.AnySlot [])
       ]
 
 ---------------------------------------------------------------
