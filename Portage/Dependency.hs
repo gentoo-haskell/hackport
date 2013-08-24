d786d0352d0c5ef3be001e51cd205bcbb3398123
Sergei Trofimovich <slyfox@gentoo.org>
2013-08-24 12:05:55 +0300

    Portage/Dependency.hs: added basic simplification passes
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- Portage/Dependency.hs
+++ Portage/Dependency.hs
@@ -107,14 +107,11 @@ is_empty_dependency (DependAnyOf deps)      = all is_empty_dependency deps
 is_empty_dependency (DependAllOf deps)      = all is_empty_dependency deps
 is_empty_dependency d@(Atom _pn _dr _dattr) = False
 
--- remove one layer of derundancy
--- currently implemented removal of
---  * empty nodes from any part of subtree
---  TODO: implement flatting (if not done yet in other phases)
---    DependAnyOf [DependAnyOf [something], rest] -> DependAnyOf $ something ++ rest
---    DependAllOf [DependAllOf [something], rest] -> DependAllOf $ something ++ rest
+-- remove one layer of redundancy
 normalization_step :: Dependency -> Dependency
-normalization_step d =
+normalization_step = combine_atoms . flatten . remove_empty
+remove_empty :: Dependency -> Dependency
+remove_empty d =
     case d of
         -- drop full empty nodes
         d | is_empty_dependency d -> empty_dependency
@@ -124,6 +121,29 @@ normalization_step d =
         -- no change
         _                         -> d
 
+-- TODO: implement flatting (if not done yet in other phases)
+--   DependAnyOf [DependAnyOf [something], rest] -> DependAnyOf $ something ++ rest
+--   DependAllOf [DependAllOf [something], rest] -> DependAllOf $ something ++ rest
+flatten :: Dependency -> Dependency
+flatten = id
+
+-- TODO: join atoms with different version constraints
+-- DependAllOf [ DRange ">=foo-1" Inf, Drange Zero "<foo-2" ] -> DRange ">=foo-1" "<foo-2"
+combine_atoms :: Dependency -> Dependency
+combine_atoms d =
+    case d of
+        (DependIfUse use dep) -> DependIfUse use (combine_atoms dep)
+        (DependAllOf deps)    -> DependAllOf $ map combine_atoms $ find_intersections  deps
+        (DependAnyOf deps)    -> DependAnyOf $ map combine_atoms $ find_concatenations deps
+        (Atom _pn _dr _dattr) -> d
+
+find_intersections :: [Dependency] -> [Dependency]
+find_intersections = map merge_depends . groupBy is_mergeable
+
+-- TODO
+find_concatenations :: [Dependency] -> [Dependency]
+find_concatenations = id
+
 -- remove various types of redundancy
 normalize_depend :: Dependency -> Dependency
 normalize_depend d = d''
@@ -131,12 +151,22 @@ normalize_depend d = d''
           d'' | d == d'   =                    d
               | otherwise = normalization_step d
 
-mergeAtomRanges :: Dependency -> Dependency -> Dependency 
-mergeAtomRanges (Atom lp ld la) (Atom rp rd ra)
-    | lp /= rp = error "mergeAtomRanges got different 'PackageName's"
-    | la /= ra = error "mergeAtomRanges got different 'DAttr's"
+-- TODO: be able to merge
+--     [use? ( a ), use? ( b ) ] -> use? ( a b )
+is_mergeable :: Dependency -> Dependency -> Bool
+is_mergeable (Atom lpn _ldrange lattr) (Atom rpn _rdrange rattr) = (lpn, lattr) == (rpn, rattr)
+is_mergeable _                         _                         = False
+
+merge_depends :: [Dependency] -> Dependency
+merge_depends [x] = x
+merge_depends xs = foldl1 merge_pair xs
+
+merge_pair :: Dependency -> Dependency -> Dependency
+merge_pair (Atom lp ld la) (Atom rp rd ra)
+    | lp /= rp = error "merge_pair got different 'PackageName's"
+    | la /= ra = error "merge_pair got different 'DAttr's"
     | otherwise = Atom lp (mergeDRanges ld rd) la
-mergeAtomRanges l r = error $ unwords ["mergeAtomRanges can't merge non-atoms:", show l, show r]
+merge_pair l r = error $ unwords ["merge_pair can't merge non-atoms:", show l, show r]
 
 dep2str :: Int -> Dependency -> String
 dep2str start_indent = render . nest start_indent . showDepend . normalize_depend
@@ -182,10 +212,12 @@ showDependInAnyOf d@(Atom _pn (DRange lb ub) _dattr)
 -- rest are fine
 showDependInAnyOf d                    =          showDepend d
 
+-- TODO: remove it in favour of more robust 'normalize_depend'
 simplify_group :: [Dependency] -> Dependency
 simplify_group [x] = x
-simplify_group xs = foldl1 mergeAtomRanges xs
+simplify_group xs = foldl1 merge_pair xs
 
+-- TODO: remove it in favour of more robust 'normalize_depend'
 -- divide packages to groups (by package name), simplify groups, merge again
 simplify_deps :: [Dependency] -> [Dependency]
 simplify_deps deps = flattenDep $ 
--- tests/print_deps.hs
+++ tests/print_deps.hs
@@ -1,25 +1,29 @@
 import Control.Monad (forM_)
+import Data.List
 
 import Test.HUnit
 
-import qualified Portage.Dependency as Portage
-import qualified Portage.Use        as Portage
+import qualified Portage.Dependency as P
+import qualified Portage.PackageId  as P
+import qualified Portage.Use        as P
+import qualified Portage.Version    as P
 import qualified RunTests as RT
 
 tests = TestList [ TestLabel "print_empty" test_print_empty
+                 , TestLabel "print_mixed" test_print_mixed
                  ]
 
 test_print_empty :: Test
 test_print_empty = TestCase $ do
     let expect_empty = ""
-        d_all = Portage.DependAllOf
-        d_any = Portage.DependAnyOf
-        d_use u dep = Portage.DependIfUse (Portage.mkUse u) dep
+        d_all = P.DependAllOf
+        d_any = P.DependAnyOf
+        d_use u dep = P.DependIfUse (P.mkUse u) dep
         deps     = [ d_all []
                    , d_any []
                    , d_use "f" (d_all [])
-                   , Portage.DependAllOf [d_any []]
-                   , Portage.DependAnyOf [d_all []]
+                   , P.DependAllOf [d_any []]
+                   , P.DependAnyOf [d_all []]
                    -- Deep Useless Use Tree :]
                    , d_use "f" $
                        d_use "g" $
@@ -35,9 +39,47 @@ test_print_empty = TestCase $ do
                                  ]
                    ]
     forM_ deps $ \d ->
-        let actual_result = Portage.dep2str 0 d
+        let actual_result = P.dep2str 0 d
         in assertEqual ("expecting empty result for " ++ show d)
                        expect_empty
                        actual_result
+
+test_print_mixed :: Test
+test_print_mixed = TestCase $ do
+    let pn = P.mkPackageName "dev-haskell" "mtl"
+        def_attr = P.DAttr P.AnySlot []
+        p_v v = P.Version { P.versionNumber   = v
+                          , P.versionChar     = Nothing
+                          , P.versionSuffix   = []
+                          , P.versionRevision = 0
+                          }
+        d_all = P.DependAllOf
+        d_any = P.DependAnyOf
+        d_ge v = P.Atom pn
+                        (P.DRange (P.NonstrictLB $ p_v v) P.InfinityB)
+                        def_attr
+        d_lt v = P.Atom pn
+                        (P.DRange P.ZeroB (P.StrictUB $ p_v v))
+                        def_attr
+        deps  = [ -- from agda: "mtl ==2.0.* || >=2.1.1 && <2.2"
+                  ( d_all [ d_any [ d_all [ d_ge [2, 0]
+                                          , d_lt [2, 1]
+                                          ]
+                                  , d_all [ d_ge [2, 1, 1]
+                                          , d_lt [2, 2]
+                                          ]
+                                  ]
+                          ]
+                  , [ "|| ( ( >=dev-haskell/mtl-2.0 <dev-haskell/mtl-2.1 )"
+                    , "     ( >=dev-haskell/mtl-2.1.1 <dev-haskell/mtl-2.2 ) )"
+                    ]
+                  )
+                ]
+    forM_ deps $ \(d, expected) ->
+        let actual = P.dep2str 0 d
+        in assertEqual ("expecting empty result for " ++ show d)
+                       actual
+                       (intercalate "\n" expected)
+
 
 main = RT.run_tests tests
