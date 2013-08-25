5f2763158503cb438f932e78c659c16272b66b97
Sergei Trofimovich <slyfox@gentoo.org>
2013-08-25 23:31:00 +0300

    tests/print_deps.hs: added pretty-printing for denorm case
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- tests/print_deps.hs
+++ tests/print_deps.hs
@@ -6,11 +6,12 @@ import Test.HUnit
 import qualified Portage.Dependency as P
 import qualified Portage.PackageId  as P
 import qualified Portage.Use        as P
-import qualified Portage.Version    as P
 import qualified RunTests as RT
 
+tests :: Test
 tests = TestList [ TestLabel "print_empty" test_print_empty
                  , TestLabel "print_mixed" test_print_mixed
+                 , TestLabel "print_denorm" test_print_denorm
                  ]
 
 test_print_empty :: Test
@@ -86,5 +87,53 @@ test_print_mixed = TestCase $ do
                        (intercalate "\n" expected)
                        actual
 
+test_print_denorm :: Test
+test_print_denorm = TestCase $ do
+    let pn = P.mkPackageName "dev-haskell" "mtl"
+        def_attr = P.DAttr P.AnySlot []
+        p_v v = P.Version { P.versionNumber   = v
+                          , P.versionChar     = Nothing
+                          , P.versionSuffix   = []
+                          , P.versionRevision = 0
+                          }
+        d_all = P.DependAllOf
+        d_any = P.DependAnyOf
+        d_ge v = P.Atom pn
+                        (P.DRange (P.NonstrictLB $ p_v v) P.InfinityB)
+                        def_attr
+        d_lt v = P.Atom pn
+                        (P.DRange P.ZeroB (P.StrictUB $ p_v v))
+                        def_attr
+        deps  = [ -- from agda: "mtl ==2.0.* || >=2.1.1 && <2.2"
+                  ( d_all [ d_any [ d_all [ d_ge [2, 0]
+                                          , d_lt [2, 1]
+                                          ]
+                                  , d_all [ d_ge [2, 1, 1]
+                                          , d_lt [2, 2]
+                                          ]
+                                  ]
+                          ]
+                  , [ "|| ( ( >=dev-haskell/mtl-2.0"
+                    , "       <dev-haskell/mtl-2.1 )"
+                    , "     ( >=dev-haskell/mtl-2.1.1"
+                    , "       <dev-haskell/mtl-2.2 ) )"
+                    ]
+                  )
+                -- remove duplicate entries
+                , ( let d = d_all [d_ge [2, 0], d_lt [2, 2]]
+                    in d_all [d, d]
+                  , [ ">=dev-haskell/mtl-2.0"
+                    , "<dev-haskell/mtl-2.2"
+                    , ">=dev-haskell/mtl-2.0"
+                    , "<dev-haskell/mtl-2.2"
+                    ]
+                  )
+                ]
+    forM_ deps $ \(d, expected) ->
+        let actual = P.dep2str_denorm d
+        in assertEqual ("expecting empty result for " ++ show d)
+                       (intercalate "\n" expected)
+                       actual
 
+main :: IO ()
 main = RT.run_tests tests
