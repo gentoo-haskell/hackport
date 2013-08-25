917d0d28bc3d979bd71aa1da33a59a1a42067c05
Sergei Trofimovich <slyfox@gentoo.org>
2013-08-25 23:02:28 +0300

    Portage/Dependency.hs: move stringifiers to 'Portage/Dependency/Print.hs'
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- Portage/Dependency.hs
+++ Portage/Dependency.hs
@@ -3,52 +3,28 @@ module Portage.Dependency
     simplify_deps
   , simplifyUseDeps
   , sortDeps
-  , dep2str
+
   -- reexports
-  , module Portage.Dependency.Types
   , module Portage.Dependency.Builder
+  , module Portage.Dependency.Print
+  , module Portage.Dependency.Types
   ) where
 
-import Portage.Version
-import Portage.Use
+import Data.Function ( on )
+import Data.List ( nub, groupBy, partition, sortBy )
+import Data.Maybe ( fromJust, mapMaybe )
+import Data.Ord           ( comparing )
 
 import Portage.PackageId
 
-import Distribution.Text ( Text(..) )
-import qualified Text.PrettyPrint as Disp
-import Text.PrettyPrint ( (<>), vcat, nest, render )
-
-import Data.Function ( on )
-import Data.Maybe ( fromJust, mapMaybe )
-import Data.List ( nub, groupBy, partition, sortBy )
-import Data.Ord           ( comparing )
-
 import Portage.Dependency.Builder
-import Portage.Dependency.Normalize
+import Portage.Dependency.Print
 import Portage.Dependency.Types
-
-dispSlot :: SlotDepend -> Disp.Doc
-dispSlot AnySlot          = Disp.empty
-dispSlot AnyBuildTimeSlot = Disp.text ":="
-dispSlot (GivenSlot slot) = Disp.text (':' : slot)
-
-dispLBound :: PackageName -> LBound -> Disp.Doc
-dispLBound pn (StrictLB    v) = Disp.char '>' <> disp pn <-> disp v
-dispLBound pn (NonstrictLB v) = Disp.text ">=" <> disp pn <-> disp v
-dispLBound _pn ZeroB = error "unhandled 'dispLBound ZeroB'"
-
-dispUBound :: PackageName -> UBound -> Disp.Doc
-dispUBound pn (StrictUB    v) = Disp.char '<' <> disp pn <-> disp v
-dispUBound pn (NonstrictUB v) = Disp.text "<=" <> disp pn <-> disp v
-dispUBound _pn InfinityB = error "unhandled 'dispUBound Infinity'"
 
 mergeDRanges :: DRange -> DRange -> DRange
 mergeDRanges _ r@(DExact _) = r
 mergeDRanges l@(DExact _) _ = l
 mergeDRanges (DRange ll lu) (DRange rl ru) = DRange (max ll rl) (min lu ru)
-
-dispDAttr :: DAttr -> Disp.Doc
-dispDAttr (DAttr s u) = dispSlot s <> dispUses u
 
 merge_pair :: Dependency -> Dependency -> Dependency
 merge_pair (Atom lp ld la) (Atom rp rd ra)
@@ -56,50 +32,6 @@ merge_pair (Atom lp ld la) (Atom rp rd ra)
     | la /= ra = error "merge_pair got different 'DAttr's"
     | otherwise = Atom lp (mergeDRanges ld rd) la
 merge_pair l r = error $ unwords ["merge_pair can't merge non-atoms:", show l, show r]
-
-dep2str :: Int -> Dependency -> String
-dep2str start_indent = render . nest start_indent . showDepend . normalize_depend
-
-(<->) :: Disp.Doc -> Disp.Doc -> Disp.Doc
-a <-> b = a <> Disp.char '-' <> b
-
-sp :: Disp.Doc
-sp = Disp.char ' '
-
-sparens :: Disp.Doc -> Disp.Doc
-sparens doc = Disp.parens (sp <> valign doc <> sp)
-
-valign :: Disp.Doc -> Disp.Doc
-valign d = nest 0 d
-
-showDepend :: Dependency -> Disp.Doc
-showDepend (Atom pn range dattr)
-    = case range of
-        -- any version
-        DRange ZeroB InfinityB -> disp pn          <> dispDAttr dattr
-        DRange ZeroB ub        -> dispUBound pn ub <> dispDAttr dattr
-        DRange lb InfinityB    -> dispLBound pn lb <> dispDAttr dattr
-        -- TODO: handle >=foo-0    special case
-        -- TODO: handle =foo-x.y.* special case
-        DRange lb ub          ->    showDepend (Atom pn (DRange lb InfinityB) dattr)
-                                 <> Disp.char ' '
-                                 <> showDepend (Atom pn (DRange ZeroB ub)    dattr)
-        DExact v              -> Disp.char '~' <> disp pn <-> disp v { versionRevision = 0 } <> dispDAttr dattr
-
-showDepend (DependIfUse u dep)  = disp u         <> sp <> sparens (showDepend dep)
-showDepend (DependAnyOf deps)   = Disp.text "||" <> sp <> sparens (vcat $ map showDependInAnyOf deps)
-showDepend (DependAllOf deps)   = valign $ vcat $ map showDepend deps
-
--- needs special grouping
-showDependInAnyOf :: Dependency -> Disp.Doc
-showDependInAnyOf d@(DependAllOf _deps) = sparens (showDepend d)
--- both lower and upper bounds are present thus needs 2 atoms
--- TODO: '=foo-x.y.*' will take only one atom, not two
-showDependInAnyOf d@(Atom _pn (DRange lb ub) _dattr)
-    | lb /= ZeroB && ub /= InfinityB
-                                       = sparens (showDepend d)
--- rest are fine
-showDependInAnyOf d                    =          showDepend d
 
 -- TODO: remove it in favour of more robust 'normalize_depend'
 simplify_group :: [Dependency] -> Dependency
@@ -131,21 +63,7 @@ getPackage (DependAllOf _dependency) = Nothing
 getPackage (Atom pn _dr _attrs) = Just pn
 getPackage (DependAnyOf _dependency           ) = Nothing
 getPackage (DependIfUse  _useFlag    _Dependency) = Nothing
-{-
-getUses  :: Dependency -> Maybe [UseFlag]
-getUses (DependAllOf _d) = Nothing
-getUses (Atom _pn _dr (DAttr _s u)) = Just u
-getUses (DependAnyOf _d) = Nothing
-getUses (DependIfUse _u _d) = Nothing
 
-getSlot :: Dependency -> Maybe SlotDepend
-getSlot (DependAllOf _d) = Nothing
-getSlot (Atom _pn _dr (DAttr s _u)) = Just s
-getSlot (DependAnyOf _d) = Nothing
-getSlot (DependIfUse _u _d) = Nothing
--}
-
---
 getPackagePart :: Dependency -> PackageName
 getPackagePart dep = fromJust (getPackage dep)
 
---
+++ Portage/Dependency/Print.hs
@@ -0,0 +1,78 @@
+module Portage.Dependency.Print
+  (
+    dep2str
+  ) where
+
+import Portage.Version
+import Portage.Use
+
+import Portage.PackageId
+
+import Distribution.Text ( Text(..) )
+import qualified Text.PrettyPrint as Disp
+import Text.PrettyPrint ( (<>), vcat, nest, render )
+
+import Portage.Dependency.Normalize
+import Portage.Dependency.Types
+
+dispSlot :: SlotDepend -> Disp.Doc
+dispSlot AnySlot          = Disp.empty
+dispSlot AnyBuildTimeSlot = Disp.text ":="
+dispSlot (GivenSlot slot) = Disp.text (':' : slot)
+
+dispLBound :: PackageName -> LBound -> Disp.Doc
+dispLBound pn (StrictLB    v) = Disp.char '>' <> disp pn <-> disp v
+dispLBound pn (NonstrictLB v) = Disp.text ">=" <> disp pn <-> disp v
+dispLBound _pn ZeroB = error "unhandled 'dispLBound ZeroB'"
+
+dispUBound :: PackageName -> UBound -> Disp.Doc
+dispUBound pn (StrictUB    v) = Disp.char '<' <> disp pn <-> disp v
+dispUBound pn (NonstrictUB v) = Disp.text "<=" <> disp pn <-> disp v
+dispUBound _pn InfinityB = error "unhandled 'dispUBound Infinity'"
+
+dispDAttr :: DAttr -> Disp.Doc
+dispDAttr (DAttr s u) = dispSlot s <> dispUses u
+
+dep2str :: Int -> Dependency -> String
+dep2str start_indent = render . nest start_indent . showDepend . normalize_depend
+
+(<->) :: Disp.Doc -> Disp.Doc -> Disp.Doc
+a <-> b = a <> Disp.char '-' <> b
+
+sp :: Disp.Doc
+sp = Disp.char ' '
+
+sparens :: Disp.Doc -> Disp.Doc
+sparens doc = Disp.parens (sp <> valign doc <> sp)
+
+valign :: Disp.Doc -> Disp.Doc
+valign d = nest 0 d
+
+showDepend :: Dependency -> Disp.Doc
+showDepend (Atom pn range dattr)
+    = case range of
+        -- any version
+        DRange ZeroB InfinityB -> disp pn          <> dispDAttr dattr
+        DRange ZeroB ub        -> dispUBound pn ub <> dispDAttr dattr
+        DRange lb InfinityB    -> dispLBound pn lb <> dispDAttr dattr
+        -- TODO: handle >=foo-0    special case
+        -- TODO: handle =foo-x.y.* special case
+        DRange lb ub          ->    showDepend (Atom pn (DRange lb InfinityB) dattr)
+                                 <> Disp.char ' '
+                                 <> showDepend (Atom pn (DRange ZeroB ub)    dattr)
+        DExact v              -> Disp.char '~' <> disp pn <-> disp v { versionRevision = 0 } <> dispDAttr dattr
+
+showDepend (DependIfUse u dep)  = disp u         <> sp <> sparens (showDepend dep)
+showDepend (DependAnyOf deps)   = Disp.text "||" <> sp <> sparens (vcat $ map showDependInAnyOf deps)
+showDepend (DependAllOf deps)   = valign $ vcat $ map showDepend deps
+
+-- needs special grouping
+showDependInAnyOf :: Dependency -> Disp.Doc
+showDependInAnyOf d@(DependAllOf _deps) = sparens (showDepend d)
+-- both lower and upper bounds are present thus needs 2 atoms
+-- TODO: '=foo-x.y.*' will take only one atom, not two
+showDependInAnyOf d@(Atom _pn (DRange lb ub) _dattr)
+    | lb /= ZeroB && ub /= InfinityB
+                                       = sparens (showDepend d)
+-- rest are fine
+showDependInAnyOf d                    =          showDepend d
