241ac0d6eb3410cbb7f2bf43c71fc9c4551bdd82
Sergei Trofimovich <slyfox@gentoo.org>
2014-01-01 21:31:03 +0300

    Portage/EBuild.hs: update copyringht up to 2014
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- Portage/EBuild.hs
+++ Portage/EBuild.hs
@@ -81,7 +81,7 @@ src_uri e =
 
 showEBuild :: EBuild -> String
 showEBuild ebuild =
-  ss "# Copyright 1999-2013 Gentoo Foundation". nl.
+  ss "# Copyright 1999-2014 Gentoo Foundation". nl.
   ss "# Distributed under the terms of the GNU General Public License v2". nl.
   ss "# $Header: $". nl.
   nl.
