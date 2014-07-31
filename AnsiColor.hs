58c6ff744f743521af167523257b77750e2021ae
Sergei Trofimovich <slyfox@gentoo.org>
2014-07-31 19:06:09 +0300

    AnsiColor.hs: cleanup exported bits
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- AnsiColor.hs
+++ AnsiColor.hs
@@ -6,7 +6,10 @@
 -}
 
 module AnsiColor
-  where
+  ( Color(..)
+  , bold
+  , inColor
+  ) where
 
 import Data.List
 
@@ -33,30 +36,11 @@ col fg bf bg = show (fromEnum fg + 30) : bf' [show (fromEnum bg + 40)]
 inColor :: Color -> Bool -> Color -> String -> String
 inColor c bf bg txt = esc (col c bf bg) ++ txt ++ esc ["00"]
 
-bold, italic, underline, inverse :: String -> String
-bold      = ansi "1" "22"
-italic    = ansi "3" "23"
-underline = ansi "4" "24"
-inverse   = ansi "7" "27"
+bold :: String -> String
+bold = ansi "1" "22"
+-- italic    = ansi "3" "23"
+-- underline = ansi "4" "24"
+-- inverse   = ansi "7" "27"
 
 ansi :: String -> String -> String -> String
 ansi on off txt = esc [on] ++ txt ++ esc [off]
-
-{-
-data Doc = Doc (Bool -> String -> String)
-
-char chr = Doc (\_ c -> chr:c)
-
-text str = Doc (\_ c -> str ++ c)
-
-(Doc t) <> (Doc u) = Doc (\b c -> t b (u b c))
-
-t <+> u = t <> char ' ' <> u
-
-showDoc (Doc d) b = d b ""
-
-color (Doc d) color = Doc (\ b c ->
-    if not b
-        then d b c
-        else inColor color False Default (d b ""))
--}
