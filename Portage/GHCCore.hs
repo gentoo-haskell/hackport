d8d5afaad490badcfce829a23825a71d1e024e9e
Jack Todaro <jack.todaro@posteo.net>
2020-01-31 17:22:07 +1100

    Portage/GHCCore.hs: port to Cabal-3
    The type signatures of lookupDependency and Dependency were changed
    in Cabal-3. Port our use of these functions to comply with the changes.
    
    Signed-off-by: Jack Todaro <jack.todaro@posteo.net>

--- Portage/GHCCore.hs
+++ Portage/GHCCore.hs
@@ -21,6 +21,7 @@ import Distribution.PackageDescription.Configuration
 import Distribution.Compiler (CompilerId(..), CompilerFlavor(GHC))
 import Distribution.System
 import Distribution.Types.ComponentRequestedSpec (defaultComponentRequestedSpec)
+import Distribution.Types.Dependency (depPkgName, depVerRange)
 
 import Distribution.Pretty (prettyShow)
 
@@ -66,9 +67,9 @@ packageIsCoreInAnyGHC pn = any (flip packageIsCore pn) (map snd ghcs)
 -- Packages that are not core will always be accepted, packages that are
 -- core in any ghc must be satisfied by the 'PackageIndex'.
 dependencySatisfiable :: InstalledPackageIndex -> Cabal.Dependency -> Bool
-dependencySatisfiable pindex dep@(Cabal.Dependency pn _rang)
+dependencySatisfiable pindex dep@(Cabal.Dependency pn _rang _lib)
   | Cabal.unPackageName pn == "Win32" = False -- only exists on windows, not in linux
-  | not . null $ lookupDependency pindex dep = True -- the package index satisfies the dep
+  | not . null $ lookupDependency pindex (depPkgName dep) (depVerRange dep) = True -- the package index satisfies the dep
   | packageIsCoreInAnyGHC pn = False -- some other ghcs support the dependency
   | otherwise = True -- the dep is not related with core packages, accept the dep
 
