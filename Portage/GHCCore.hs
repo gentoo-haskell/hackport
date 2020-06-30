a839d3f368dadf0693287280f3d6e0768863b8a9
Jack Todaro <solpeth@posteo.org>
2020-07-01 06:27:58 +1000

    Portage/GHCCore: add ghc-8.8.3 library detection
    Signed-off-by: Jack Todaro <solpeth@posteo.org>

--- Portage/GHCCore.hs
+++ Portage/GHCCore.hs
@@ -34,7 +34,7 @@ import Debug.Trace
 -- It means that first ghc in this list is a minimum default.
 ghcs :: [(DC.CompilerInfo, InstalledPackageIndex)]
 ghcs = modern_ghcs
-    where modern_ghcs  = [ghc741, ghc742, ghc762, ghc782, ghc7101, ghc7102, ghc801, ghc802, ghc821, ghc843, ghc863, ghc865, ghc881, ghc8101]
+    where modern_ghcs  = [ghc741, ghc742, ghc762, ghc782, ghc7101, ghc7102, ghc801, ghc802, ghc821, ghc843, ghc863, ghc865, ghc881, ghc883, ghc8101]
 
 cabalFromGHC :: [Int] -> Maybe Cabal.Version
 cabalFromGHC ver = lookup ver table
@@ -51,6 +51,7 @@ cabalFromGHC ver = lookup ver table
           , ([8,6,3],  Cabal.mkVersion [2,4,0,1])
           , ([8,6,5],  Cabal.mkVersion [2,4,0,1])
           , ([8,8,1],  Cabal.mkVersion [3,0,0,0])
+          , ([8,8,3],  Cabal.mkVersion [3,0,1,0])
           , ([8,10,1], Cabal.mkVersion [3,2,0,0])
           ]
 
@@ -123,6 +124,9 @@ ghc nrs = DC.unknownCompilerInfo c_id DC.NoAbiTag
 
 ghc8101 :: (DC.CompilerInfo, InstalledPackageIndex)
 ghc8101 = (ghc [8,10,1], mkIndex ghc8101_pkgs)
+
+ghc883 :: (DC.CompilerInfo, InstalledPackageIndex)
+ghc883 = (ghc [8,8,3], mkIndex ghc883_pkgs)
 
 ghc881 :: (DC.CompilerInfo, InstalledPackageIndex)
 ghc881 = (ghc [8,8,1], mkIndex ghc881_pkgs)
@@ -204,6 +208,38 @@ ghc8101_pkgs =
 --  , p "xhtml" [3000,2,2,1]
   ]
 
+ghc883_pkgs :: [Cabal.PackageIdentifier]
+ghc883_pkgs =
+  [ p "array" [0,5,4,0]
+  , p "base" [4,13,0,0]
+  , p "binary" [0,8,7,0] -- used by libghc
+  , p "bytestring" [0,10,10,0]
+--  , p "Cabal" [3,0,1,0]  package is upgradeable
+  , p "containers" [0,6,2,1]
+  , p "deepseq" [1,4,4,0] -- used by time
+  , p "directory" [1,3,6,0]
+  , p "filepath" [1,4,2,1]
+  , p "ghc-boot" [8,8,3]
+  , p "ghc-boot-th" [8,8,3]
+  , p "ghc-compact" [0,1,0,0]
+  , p "ghc-prim" [0,5,3,0]
+  , p "ghci" [8,8,3]
+--  , p "haskeline" [0,7,5,0]  package is upgradeable
+  , p "hpc" [0,6,0,3] -- used by libghc
+  , p "integer-gmp" [1,0,2,0]
+  --  , p "mtl" [2,2,2]  package is upgradeable(?)
+  --  , p "parsec" [3,1,14,0]  package is upgradeable(?)
+  , p "pretty" [1,1,3,6]
+  , p "process" [1,6,8,0]
+  --  , p "stm" [2,5,0,0]  package is upgradeable(?)
+  , p "template-haskell" [2,15,0,0] -- used by libghc
+  -- , p "terminfo" [0,4,1,4]
+  -- , p "text" [1,2,4,0] dependency of Cabal library
+  , p "time" [1,9,3,0] -- used by unix, directory, hpc, ghc. unsafe to upgrade
+  , p "transformers" [0,5,6,2] -- used by libghc
+  , p "unix" [2,7,2,2]
+--  , p "xhtml" [3000,2,2,1]
+  ]
 
 ghc881_pkgs :: [Cabal.PackageIdentifier]
 ghc881_pkgs =
