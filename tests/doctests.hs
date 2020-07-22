3a3046a467ff8d6a1ee5ffb9acc81d6bcce5ab5c
Jack Todaro <solpeth@posteo.org>
2020-07-23 07:47:02 +1000

    doctests.hs: remove Merge/Utils.hs from tests
    Signed-off-by: Jack Todaro <solpeth@posteo.org>

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -4,8 +4,7 @@ import Test.DocTest
 -- Some modules fail to find module Paths_hackport, which is generated during building.
 -- There is a way to make this work properly, but for now just test modules which
 -- don't import Paths_hackport.
-main = doctest [ "Merge/Utils.hs"
-               , "Portage/Cabal.hs"
+main = doctest [ "Portage/Cabal.hs"
                , "Portage/GHCCore.hs"
                , "Portage/PackageId.hs"
                , "Portage/Version.hs"
