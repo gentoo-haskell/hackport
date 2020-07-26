4264a8c688c776eb38d58354f2facbd463d255fd
Jack Todaro <solpeth@posteo.org>
2020-07-27 06:32:11 +1000

    QuickCheck/Instances.hs: further randomise Arbitrary instances
    The Arbitrary ValidSuffix instance will now generate a random
    integer number for the Suffix, whereas previously it was hard-coded
    as 1.
    
    As alluded to above, AnySuffix has been renamed to ValidSuffix for
    clarity, and an explicit module export list has been added.
    
    Signed-off-by: Jack Todaro <solpeth@posteo.org>

--- tests/QuickCheck/Instances.hs
+++ tests/QuickCheck/Instances.hs
@@ -1,4 +1,4 @@
-module QuickCheck.Instances where
+module QuickCheck.Instances (ComplexVersion(..)) where
 
 import Test.QuickCheck
 
@@ -9,37 +9,36 @@ import Portage.Version (Suffix(..), Version(..))
 --------------------------------------------------------------------------------
 -- | Wrapper for 'Suffix', intended for use in an 'Arbitrary' instance
 -- to return a single, valid 'Suffix'.
-newtype AnySuffix = AnySuffix { getSuffix :: Suffix }
+newtype ValidSuffix = ValidSuffix { getSuffix :: Suffix }
   deriving (Eq,Ord,Show)
 
 -- | Wrapper For 'Version', intended for use in an 'Arbitrary' instance
--- where we want to generate the most complex 'Version's possible.
+-- where we want to generate the most complex 'Version' possible.
 newtype ComplexVersion = ComplexVersion { getVersion :: Version }
   deriving (Eq,Ord,Show)
 
 --------------------------------------------------------------------------------
 -- Instances
 --------------------------------------------------------------------------------
--- | Return a single, valid 'AnySuffix'. Currently the version of the
--- 'AnySuffix' is hardcoded as @1@.
-instance Arbitrary AnySuffix where
-  arbitrary = elements $ AnySuffix <$> [ Alpha 1
-                                       , Beta  1
-                                       , Pre   1
-                                       , RC    1
-                                       , P     1
-                                       ]
-
+-- | Return a single, valid 'ValidSuffix'.
+instance Arbitrary ValidSuffix where
+  arbitrary = oneof [ ValidSuffix . Alpha . getNonNegative <$> arbitrary
+                    , ValidSuffix . Beta  . getNonNegative <$> arbitrary
+                    , ValidSuffix . Pre   . getNonNegative <$> arbitrary
+                    , ValidSuffix . RC    . getNonNegative <$> arbitrary
+                    , ValidSuffix . P     . getNonNegative <$> arbitrary
+                    ]
+  
 -- | Return a valid 'ComplexVersion' with a non-empty 'versionNumber',
--- an optional 'versionChar', a ['Suffix'] which may be empty, and
--- a 'NonNegative' 'versionRevision' which may be zero.
+-- an optional 'versionChar' if valid, a ['Suffix'] which may be empty,
+-- and a 'NonNegative' 'versionRevision' which may be zero.
 --
--- This is used to generate 'Version's of high complexity to
+-- This is used to generate a 'Version' of high complexity to
 -- stress-test our parsers for a range of valid inputs.
 instance Arbitrary ComplexVersion where
   arbitrary = do
     v <- listOf1 $ getNonNegative <$> arbitrary
     c <- Just <$> choose ('a','z')
-    s <- listOf $ getSuffix <$> (arbitrary :: Gen AnySuffix)
+    s <- listOf $ getSuffix <$> arbitrary
     (NonNegative r) <- arbitrary
     return $ ComplexVersion $ Version v (if length v == 1 then Nothing else c) s r
