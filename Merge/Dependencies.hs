9d8aec245c8825d5224dc04f1c9a01995e02aede
Sergey Alirzaev <zl29ah@gmail.com>
2021-03-19 19:32:31 +0300

    Merge/Dependencies.hs: handle ghc C library dep

--- Merge/Dependencies.hs
+++ Merge/Dependencies.hs
@@ -383,6 +383,7 @@ staticTranslateExtraLib lib = lookup lib m
       , ("SDL2_mixer", any_c_p "media-libs" "sdl2-mixer")
       , ("blas", any_c_p "virtual" "blas")
       , ("lapack", any_c_p "virtual" "lapack")
+      , ("ghc", any_c_p "dev-lang" "ghc")
       ]
 
 ---------------------------------------------------------------
