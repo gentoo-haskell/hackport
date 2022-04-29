97a30d5f7a8e1c9b113b743165d8fb94622fbc7a
hololeap <hololeap@users.noreply.github.com>
2022-04-29 12:08:54 -0600

    src/Portage/EBuild.hs: Remove obsolete 'src_uri' function
    A default SRC_URI pointing to hackage.haskell.org is now handled automatically
    by haskell-cabal.eclass
    
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- tests/Portage/EBuildSpec.hs
+++ tests/Portage/EBuildSpec.hs
@@ -6,13 +6,6 @@ import Portage.EBuild
 
 spec :: Spec
 spec = do
-  describe "src_uri" $ do
-    it "returns the default SRC_URI for a lowercase package name" $ do
-      src_uri ebuildTemplate
-        `shouldBe` "https://hackage.haskell.org/package/${P}/${P}.tar.gz"
-    it "returns the case-sensitive SRC_URI for a case-sensitive package name" $ do
-      src_uri ebuildTemplate { my_pn = Just "Foo_Bar" }
-        `shouldBe` "https://hackage.haskell.org/package/${MY_P}/${MY_P}.tar.gz"
   describe "sort_iuse" $ do
     it "sorts IUSE alphabetically despite pluses (+)" $ do
       sort_iuse ["+a","c","+b","d"] `shouldBe` ["+a","+b","c","d"]
