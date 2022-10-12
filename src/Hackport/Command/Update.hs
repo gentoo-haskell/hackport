4319680ec6dbc7199046e4155254135a7ba6935a
Miezhiko <Miezhiko@gmail.com>
2022-10-12 12:47:35 +0400

    upgrade hackport to actual Cabal / cabal-install / hackage-security TODO: fix update (who is using it anyways?)
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Hackport/Command/Update.hs
+++ src/Hackport/Command/Update.hs
@@ -1,16 +1,17 @@
 module Hackport.Command.Update
-  ( updateAction
+  ( CabalInstall.updateAction
   ) where
 
 import Distribution.Simple.Command (CommandUI(..))
 import qualified Distribution.Client.Setup as CabalInstall
-import qualified Distribution.Client.Update as CabalInstall
+import qualified Distribution.Client.CmdUpdate as CabalInstall
 
 import Hackport.Util (withHackportContext)
 import Hackport.Env
 
-updateAction :: Env env ()
-updateAction  = askGlobalEnv >>= \(GlobalEnv verbosity _ _) ->
-  withHackportContext $ \repoContext -> do
-    let updateFlags = commandDefaultFlags CabalInstall.updateCommand
-    liftIO $ CabalInstall.update verbosity updateFlags repoContext
+--updateAction :: Env env ()
+--updateAction  = askGlobalEnv >>= \(GlobalEnv verbosity _ _) ->
+--  withHackportContext $ \repoContext -> do
+    --let updateFlags = commandDefaultFlags CabalInstall.updateCommand
+    -- | TODO FIX THIS METHOD: https://github.com/Qeenon/cabal/blob/gentoo/cabal-install/src/Distribution/Client/CmdUpdate.hs
+    --liftIO $ CabalInstall.updateRepo verbosity updateFlags repoContext reposToUpdate
