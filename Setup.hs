53512b2292c2edd5d8b4f588817f7350b3eb1223
hololeap <hololeap@users.noreply.github.com>
2023-04-02 12:52:43 -0600

    Allow Setup.hs to work without cabal-doctest
    Use CPP pragmas to switch between cabal-doctest and standard Setup,
    depending on if cabal-doctest is installed.
    
    Bug: https://github.com/gentoo-haskell/gentoo-haskell/issues/1403
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
+
+#ifndef MIN_VERSION_cabal_doctest
+#define MIN_VERSION_cabal_doctest(x,y,z) 0
+#endif
+
+#if MIN_VERSION_cabal_doctest(1,0,0)
 
 import Distribution.Extra.Doctest ( defaultMainWithDoctests )
 
 main :: IO ()
 main = defaultMainWithDoctests "doctests"
+
+#else
+
+import Distribution.Simple
+
+main :: IO ()
+main = defaultMain
+
+#endif
