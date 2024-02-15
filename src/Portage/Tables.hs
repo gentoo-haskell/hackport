877cdc0ade474b788e5a976f65a346518dda5a13
hololeap <hololeap@protonmail.com>
2024-02-14 22:31:07 -0700

    Portage.Tables: Simplify module
    Two packages were handled specially by hackport since they had non-0
    slots. Now that those packages have been switched to a 0 slot, the
    code to specially handle them is no longer needed.
    
    In the future, there shouldn't be any other packages with non-0 slots,
    so the code in Portage.Tables should be removed.
    
    This is a correction of the mistake that was made in
    bb0f72ded553b0fe79f8b7ac1678e100900869f8.
    
    Bug: https://github.com/gentoo-haskell/gentoo-haskell/issues/1502
    Signed-off-by: hololeap <hololeap@protonmail.com>

--- src/Portage/Tables.hs
+++ src/Portage/Tables.hs
@@ -11,27 +11,9 @@ module Portage.Tables
 
 import Portage.Dependency.Builder
 import Portage.Dependency.Types
-import Portage.PackageId
-
-import Data.Monoid
 
 -- | Set the @SLOT@ for a given 'Dependency'.
 set_build_slot :: Dependency -> Dependency
-set_build_slot = 
-  overAtom $ \a@(Atom pn dr (DAttr _ u)) -> 
-      case mconcat $ map (First . matches a) slottedPkgs of
-          First (Just s) -> Atom pn dr (DAttr s u)
-          First Nothing  -> Atom pn dr (DAttr AnyBuildTimeSlot u)
-    where
-      matches (Atom pn _ _) (nm,s) 
-        | pn == nm  = Just s
-        | otherwise = Nothing
-
--- | List of 'PackageName's with their corresponding default 'SlotDepend's.
---
--- For example, dependency @QuickCheck@ has its @SLOT@ always set to @2@.
-slottedPkgs :: [(PackageName, SlotDepend)]
-slottedPkgs =
-  [ (mkPackageName "dev-haskell" "quickcheck", GivenSlot "2=")
-  , (mkPackageName "dev-haskell" "hdbc", GivenSlot "2=")
-  ]
+set_build_slot =
+  overAtom $ \(Atom pn dr (DAttr _ u)) ->
+      Atom pn dr (DAttr AnyBuildTimeSlot u)
