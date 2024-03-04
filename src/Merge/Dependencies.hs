6be4ae183bc3fc0c8cec813f24c9c2448cdd5fb5
Sergey Alirzaev <l29ah@riseup.net>
2024-03-04 17:50:54 +0100

    Merge.Dependencies: + double-conversion

--- src/Merge/Dependencies.hs
+++ src/Merge/Dependencies.hs
@@ -394,6 +394,7 @@ staticTranslateExtraLib lib = lookup lib m
       , ("libbrotlidec", any_c_p "app-arch" "brotli")
       , ("libbrotlienc", any_c_p "app-arch" "brotli")
       , ("libarchive", at_least_c_p_v "app-arch" "libarchive" [3,5,2])
+      , ("double-conversion", any_c_p "dev-libs" "double-conversion")
       ]
 
 ---------------------------------------------------------------
